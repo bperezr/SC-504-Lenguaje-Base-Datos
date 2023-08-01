a80b76262224656eb485dc2d3b615c4b37d02336
Bryan Perez <bperezr@outlook.com>
2023-07-31 19:50:54 -0600

    Se agrega la funcion Delete a eventos

--- admin_events.php
+++ admin_events.php
@@ -1,10 +1,38 @@
-<?php 
+<?php
+/* 
+echo "<pre>";
+ var_dump($_POST);
+echo "</pre>"; */
+
 require 'include/database/database.php';
 $db = ConectarDB();
 
-$queryEventos = "SELECT * FROM eventos ";
+$queryEventos = "SELECT * FROM eventos";
 
 $result = mysqli_query($db, $queryEventos);
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['idEvento'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if ($id) {
+        //Eliminar imagen
+        $queryImagen = "SELECT imagen FROM eventos where idEvento = ${id}";
+        $resultImg = mysqli_query($db, $queryImagen);
+        $eventoImagen = mysqli_fetch_assoc($resultImg);
+
+        unlink('img/images/' . $eventoImagen['imagen']);
+
+        //Eliminar evento
+        $queryDelete = "DELETE from eventos where idEvento = ${id}";
+        $result = mysqli_query($db, $queryDelete);
+
+        if ($result) {
+            header('Location: /SC-502-Proyecto/admin_events.php');
+        }
+    }
+}
 
 ?>
 
@@ -44,38 +72,42 @@ $result = mysqli_query($db, $queryEventos);
                     </div>
                 </div>
                 <div class="buscador buscador_agregar">
-                        <!---Agregar-->
-                        <div class="agregar">
-                            <a href="admin_events_new.php" class="btn_agregar"><ion-icon
-                                    name="add-circle-outline"></ion-icon>
-                                Agregar</a>
-                        </div>
+                    <!---Agregar-->
+                    <div class="agregar">
+                        <a href="admin_events_new.php" class="btn_agregar"><ion-icon name="add-circle-outline"></ion-icon>
+                            Agregar</a>
                     </div>
                 </div>
+            </div>
             </div>
         </form>
 
         <section class="event__tarjetas">
-            <?php while($eventos = mysqli_fetch_assoc($result)): ?>
-            <!-- Evento 1 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/images/<?php echo $eventos['imagen']; ?>" alt="Evento 1">
+            <?php while ($eventos = mysqli_fetch_assoc($result)) : ?>
+                <!-- Evento 1 -->
+                <div class="tarjeta">
+                    <div class="tarjeta__imagen">
+                        <img src="img/images/<?php echo $eventos['imagen']; ?>" alt="Evento 1">
+                    </div>
+                    <div class="tarjeta__detalle">
+                        <h2><?php echo $eventos['nombreEvento']; ?></h2>
+                        <ul class="detalle-evento">
+                            <li><strong>Fecha:</strong><?php echo $eventos['fecha']; ?></li>
+                            <li><strong>Hora:</strong> <?php echo $eventos['hora_inicio']; ?> - <?php echo $eventos['hora_fin'] ?></li>
+                        </ul>
+                    </div>
+                    <!-- Botones -->
+                    <div class="tarjeta__btn">
+                        <a href="admin_events_edit.php?id=<?php echo $eventos['idEvento']; ?>" class="editar"><ion-icon name="create-sharp"></ion-icon>Editar</a>
+
+                        <form method="POST">
+                            <input type="hidden" name="idEvento" value="<?php echo $eventos['idEvento'] ?>">
+                            <input type="submit" class="eliminar" value="Eliminar">
+                        </form>
+
+                    </div>
                 </div>
-                <div class="tarjeta__detalle">
-                    <h2><?php echo $eventos['nombreEvento']; ?></h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong><?php echo $eventos['fecha']; ?></li>
-                        <li><strong>Hora:</strong> <?php echo $eventos['hora_inicio']; ?> - <?php echo $eventos['hora_fin'] ?></li>
-                    </ul>
-                </div>
-                <!-- Botones -->
-                <div class="tarjeta__btn">
-                    <a href="admin_events_edit.php?id=<?php echo $eventos['idEvento']; ?>" class="editar"><ion-icon name="create-sharp"></ion-icon>Editar</a>
-                    <a href="" class="eliminar"><ion-icon name="trash-sharp"></ion-icon>Eliminar</a>
-                </div>
-            </div>
-            <?php endwhile; ?>   
+            <?php endwhile; ?>
         </section>
     </main>
 
--- events.php
+++ events.php
@@ -1,12 +1,18 @@
-<?php 
+<?php
+require 'include/database/database.php';
+$db = ConectarDB();
 
+$queryEventos = "SELECT * FROM eventos ";
+
+$result = mysqli_query($db, $queryEventos);
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <!-- styles -->
-    <?php 
+    <?php
     $rutaCSS = 'css/events.css';
     include 'include/template/header.php'; ?>
 
@@ -36,108 +42,28 @@
         </section>
 
         <section class="event__tarjetas">
-            <!-- Evento 1 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento1.jpg" alt="Evento 1">
-                </div>
-                <div class="tarjeta__detalle">
-                    <h2>Campaña de Castración Felina y Canina</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 10 de agosto de 2023</li>
-                        <li><strong>Hora:</strong> 9:00 AM - 4:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
+            <?php while ($eventos = mysqli_fetch_assoc($result)) : ?>
+                <!-- Evento 1 -->
+                <div class="tarjeta">
+                    <div class="tarjeta__imagen">
+                        <img src="img/images/<?php echo $eventos['imagen']; ?>" alt="Evento 1">
+                    </div>
+                    <div class="tarjeta__detalle">
+                        <h2><?php echo $eventos['nombreEvento']; ?></h2>
+                        <ul class="detalle-evento">
+                        <li><strong>Lugar:</strong><?php echo $eventos['Lugar']; ?></li>
+                            <li><strong>Fecha:</strong><?php echo $eventos['fecha']; ?></li>
+                            <li><strong>Hora:</strong> <?php echo $eventos['hora_inicio']; ?> - <?php echo $eventos['hora_fin'] ?></li>
+                        </ul>
+                    </div>
+                    <!-- Botones -->
+                    <div class="tarjeta__btn">
                     <a href="events_post.php" class="ver-evento">Ver evento</a>
                 </div>
-            </div>
-
-            <!-- Evento 2 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento2.jpg" alt="Evento 2">
                 </div>
-                <div class="tarjeta__detalle">
-                    <h2>Día de Adopción Responsable</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 22 de septiembre de 2023</li>
-                        <li><strong>Hora:</strong> 10:00 AM - 2:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
-                    <a href="events_post.php" class="ver-evento">Ver evento</a>
-                </div>
-            </div>
-
-            <!-- Evento 3 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento3.jpg" alt="Evento 3">
-                </div>
-                <div class="tarjeta__detalle">
-                    <h2>Charla sobre Nutrición y Salud Animal</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 30 de septiembre de 2023</li>
-                        <li><strong>Hora:</strong> 6:00 PM - 8:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
-                    <a href="events_post.php" class="ver-evento">Ver evento</a>
-                </div>
-            </div>
-
-            <!-- Evento 4 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento4.jpg" alt="Evento 4">
-                </div>
-                <div class="tarjeta__detalle">
-                    <h2>Exposición de Razas Caninas</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 15 de octubre de 2023</li>
-                        <li><strong>Hora:</strong> 10:00 AM - 5:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
-                    <a href="events_post.php" class="ver-evento">Ver evento</a>
-                </div>
-            </div>
-
-            <!-- Evento 5 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento5.jpg" alt="Evento 5">
-                </div>
-                <div class="tarjeta__detalle">
-                    <h2>Vacunación Gratuita</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 25 de octubre de 2023</li>
-                        <li><strong>Hora:</strong> 9:00 AM - 1:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
-                    <a href="events_post.php" class="ver-evento">Ver evento</a>
-                </div>
-            </div>
-
-            <!-- Evento 6 -->
-            <div class="tarjeta">
-                <div class="tarjeta__imagen">
-                    <img src="img/imagen_evento6.jpg" alt="Evento 5">
-                </div>
-                <div class="tarjeta__detalle">
-                    <h2>Taller de Adiestramiento Canino</h2>
-                    <ul class="detalle-evento">
-                        <li><strong>Fecha:</strong> 5 de noviembre de 2023</li>
-                        <li><strong>Hora:</strong>  2:00 PM - 4:00 PM</li>
-                    </ul>
-                </div>
-                <div class="tarjeta__btn">
-                    <a href="events_post.php" class="ver-evento">Ver evento</a>
-                </div>
-            </div>
+            <?php endwhile; ?>
         </section>
+      
     </main>
 
     <!-- Footer -->
