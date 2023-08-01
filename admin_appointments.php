494da6637b7af6b3c3178011f1e72ab4ad4a84f5
Mathias-Vargas <mathias.vargas.ram@gmail.com>
2023-08-01 11:34:10 -0600

    create y delete de citas

--- admin_appointments.php
+++ admin_appointments.php
@@ -16,7 +16,20 @@ $queryCitas = "SELECT
                 join cliente as n on c.idCliente = n.idCliente";
 
 $result = mysqli_query($db, $queryCitas);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['idCita'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
 
+    if ($id) {
+        //Eliminar cita
+        $queryDelete = "DELETE from citas where idCita = ${id}";
+        $resultDel = mysqli_query($db, $queryDelete);
+
+        if ($resultDel) {
+            header('Location: /SC-502-Proyecto/admin_appointments.php');
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -95,7 +108,7 @@ $result = mysqli_query($db, $queryCitas);
                         <a href="admin_appointments_edit.php?id=<?php echo $cita['idCita']; ?>" class="editar"><ion-icon name="create-sharp"></ion-icon>Editar</a>
 
                         <form method="POST">
-                            <input type="hidden" name="idEvento" value="<?php echo $eventos['idEvento'] ?>">
+                            <input type="hidden" name="idCita" value="<?php echo $cita['idCita'] ?>">
                             <input type="submit" class="eliminar" value="Eliminar">
                         </form>
                     </div>
