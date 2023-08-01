17184b78a2b68406e1b25a45645d544ef5b4d94d
Mathias-Vargas <mathias.vargas.ram@gmail.com>
2023-08-01 02:22:03 -0600

    ADMIN APPOINTMENTS

---
+++ admin_appointments.php
@@ -0,0 +1,112 @@
+<?php
+
+require 'include/database/database.php';
+$db = ConectarDB();
+$queryCitas = "SELECT
+                c.*,
+                hi.horaInicio,
+                hi.horaFin,
+                m.tipo as tipoMascota,
+                s.servicio as nombreServicio,
+                n.nombre as nombreCliente
+                FROM citas as c
+                join horariocitas as hi on c.idHorario = hi.idHorario
+                join tipomascota as m on c.idMascota =  m.idTipoMascota
+                join servicios as s on c.idServicio = s.idServicio
+                join cliente as n on c.idCliente = n.idCliente";
+
+$result = mysqli_query($db, $queryCitas);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- styles -->
+    <?php $rutaCSS = 'css/admin_events.css';
+    include 'include/template/header.php'; ?>
+</head>
+
+<body>
+    <!-- Nav template -->
+    <?php $enlaceActivo = 'admin_appointments';
+    include 'include/template/nav_admin.php'; ?>
+
+    <main class="contenedor">
+
+        <h1 class="centrar-texto">Administrar Citas</h1>
+
+        <!-- Buscador -->
+        <form action="buscar" method="get">
+            <div class="contenedor_buscar">
+                <div class="buscador buscador_buscar">
+                    <!-- Texto buscar -->
+                    <div class="textBuscar">
+                        <input type="text" placeholder="Buscar..." name="search">
+                    </div>
+                    <!-- Buscar -->
+                    <div class="buscar">
+                        <button class="btn_buscar" type="submit">Buscar</button>
+                    </div>
+                    <!-- Recargar -->
+                    <div class="recargar">
+                        <a href="admin_appointments.php"><ion-icon name="refresh-circle"></ion-icon></a>
+                    </div>
+                </div>
+                <div class="buscador buscador_agregar">
+                    <!---Agregar-->
+                    <div class="agregar">
+                        <a href="admin_appointments_new.php" class="btn_agregar"><ion-icon
+                                name="add-circle-outline"></ion-icon>
+                            Agregar</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </form>
+
+        <section class="event__tarjetas">
+            <?php while ($cita = mysqli_fetch_assoc($result)): ?>
+                <!-- Evento 1 -->
+                <div class="tarjeta">
+                    <div class="tarjeta__detalle">
+                        <h2>
+                            <?php echo $cita['nombreServicio']; ?>
+                        </h2>
+                        <ul class="detalle-evento">
+                            <li><strong>Cliente:</strong>
+                                <?php echo $cita['idCliente']; ?> - <?php echo $cita['nombreCliente']; ?>
+                            </li>
+                            <li><strong>Fecha:</strong>
+                                <?php echo $cita['fecha']; ?>
+                            </li>
+                            <li><strong>Horario:</strong>
+                                <?php echo $cita['horaInicio']; ?> -
+                                <?php echo $cita['horaFin']; ?>
+                            </li>
+                            <li><strong>Mascota:</strong>
+                                <?php echo $cita['tipoMascota']; ?>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Botones -->
+                    <div class="tarjeta__btn">
+                        <a href="admin_appointments_edit.php?id=<?php echo $cita['idCita']; ?>" class="editar"><ion-icon name="create-sharp"></ion-icon>Editar</a>
+
+                        <form method="POST">
+                            <input type="hidden" name="idEvento" value="<?php echo $eventos['idEvento'] ?>">
+                            <input type="submit" class="eliminar" value="Eliminar">
+                        </form>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        </section>
+    </main>
+
+    <!-- Footer -->
+    <?php include 'include/template/footer.php'; ?>
+    <!-- JS -->
+</body>
+
+</html>
--- include/template/nav_admin.php
+++ include/template/nav_admin.php
@@ -10,6 +10,7 @@
         <ul>
             <li><a href="admin_medicals.php" <?php if ($enlaceActivo == 'admin_medicals') echo 'class="active"'; ?>>Médicos</a></li>
             <li><a href="admin_events.php" <?php if ($enlaceActivo == 'admin_events') echo 'class="active"'; ?>>Eventos</a></li>
+            <li><a href="admin_appointments.php" <?php if ($enlaceActivo == 'admin_appointments') echo 'class="active"'; ?>>Citas</a></li>
             <li class="login">
                 <p><ion-icon name="person-circle-outline"></ion-icon></p>
                 <ul>
