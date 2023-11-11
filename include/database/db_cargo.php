3c5513152028a734f889366212aa24e7112881e1
Karen J Román <84334642+UpsKaren@users.noreply.github.com>
2023-11-10 19:23:42 -0600

    Volver al Origen

--- include/database/db_cargo.php
+++ include/database/db_cargo.php
@@ -37,131 +37,76 @@ class Cargo
 
 
     // Función para obtener un cargo por su ID
-   /* public function getCargo($id)
+    public function getCargo($id)
     {
         $query = "SELECT * FROM cargo WHERE idCargo = :idCargo";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':idCargo', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
-    }*/
-
-
-
-   /*Primer SP */
-    public function getCargo($id,$resultado)
-    {
-        $sql = "BEGIN getCargo(:id, resultado); END;";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
+
+
 
 
     // Función para obtener todos los cargos
 
-    /*public function getCargos()
+    public function getCargos()
     {
         $query = "SELECT * FROM cargo";
         $stmt = $this->db->query($query);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-
-
-   /* Segundo SP*/ 
-    public function getCargos($id)
-    {
-        $sql = "BEGIN getCargos(:id); END;";
-        $stmt = $this->db->prepare($sql);
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
 
+
+ 
 
 
 
     // Función para insertar un nuevo cargo
     
  
-    /*public function insertCargo($cargo)
+    public function insertCargo($cargo)
     {
         $query = "INSERT INTO cargo (cargo) VALUES (:cargo)";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':cargo', $cargo);
         return $stmt->execute();
-    }*/
+    }
 
-  /* Tercer SP*/ 
-  public function insertCargo($cargo,$resultado)
-  {
-      $sql = "BEGIN insertCargo(:cargo, :resultado); END;";
-      $stmt = $this->db->prepare($sql);
-
-      $stmt->bindParam(':cargo', $cargo);
-      $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-
-      $stmt->execute();
-  }
 
 
 
     // Función para actualizar un cargo
 
 
-    /*public function updateCargo($id, $cargo)
+    public function updateCargo($id, $cargo)
     {
         $query = "UPDATE cargo SET cargo = :cargo WHERE idCargo = :idCargo";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':idCargo', $id);
         $stmt->bindParam(':cargo', $cargo);
         return $stmt->execute();
-    }*/
-
-/* Cuarto SP*/ 
-
-public function updateCargo($id, $cargo,&$resultado)
-    {
-        $sql = "BEGIN updateCargo(:id, :cargo, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':cargo', $cargo);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-
-        $stmt->execute();
     }
 
 
 
 
     // Función para eliminar un cargo por su ID
-    /*public function deleteCargo($id)
+    public function deleteCargo($id)
     {
         $query = "DELETE FROM cargo WHERE idCargo = :idCargo";
         $stmt = $this->db->prepare($query);
         $stmt->bindParam(':idCargo', $id);
         return $stmt->execute();
-    }*/
-
-    /* Quinto SP*/ 
-    public function deleteCargo($id, $resultado)
-    {
-        $sql = "BEGIN deleteCargo(:id, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
 
+   
 
 
 
 
-/* Sexto SP*/ 
-    /*public function buscarCargos($searchTerm)
+    public function buscarCargos($searchTerm)
     {
         $query = "SELECT c.*
                 FROM cargo c            
@@ -170,17 +115,6 @@ public function updateCargo($id, $cargo,&$resultado)
         $stmt->bindValue(':searchTerm', '%' . $searchTerm . '%', PDO::PARAM_STR);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-
-
-    public function buscarCargos($cargo, $resultado)
-    {
-        $sql = "BEGIN deleteCargo(:cargo, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':cargo', $cargo);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
 
 
--- include/database/db_servicio.php
+++ include/database/db_servicio.php
@@ -37,25 +37,13 @@ class Servicio
 
    
 
-   /*public function getServicio($id)
+   public function getServicio($id)
     {
         $query = "SELECT * FROM servicios WHERE idServicio = :id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
-    }*/
-
-//Primer SP// 
-    public function getServicio($id,$resultado)
-    {
-        $sql = "BEGIN getServicio(:id, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-
-        $stmt->execute();
     }
 
 
@@ -63,59 +51,32 @@ class Servicio
 
 
 
-   /* public function getServicios()
+
+    public function getServicios()
     {
         $query = "SELECT * FROM servicios";
         $stmt = $this->db->query($query);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }*/
-
-
-//Segundo SP// 
-public function getServicios($servicios,$resultado)
-{
-    $sql = "BEGIN getServicio(:servicios :resultado); END;";
-    $stmt = $this->db->prepare($sql);
-    $stmt->bindParam(':servicios', $servicios);
-    $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-    $stmt->execute();
-}
+    }
 
 
 
-
-
-
-
-
-
-
-    /*public function insertServicio($servicio, $descripcion)
+    public function insertServicio($servicio, $descripcion)
     {
         $query = "INSERT INTO servicios (servicio, descripcion) VALUES (:servicio, :descripcion)";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':servicio', $servicio, PDO::PARAM_STR);
         $stmt->bindValue(':descripcion', $descripcion, PDO::PARAM_STR);
         return $stmt->execute();
-    }*/
-
-    
-//Tercer SP// 
-public function insertServicio($servicio, $descripcion,&$resultado)
-    {
-        $sql = "BEGIN insertServicio(:servicio, :descripcion, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':servicio', $servicio);
-        $stmt->bindParam(':descripcion', $descripcion);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
 
+    
 
 
 
-   /* public function updateServicio($id, $servicio, $descripcion)
+
+
+   public function updateServicio($id, $servicio, $descripcion)
     {
         $query = "UPDATE servicios SET servicio = :servicio, descripcion = :descripcion WHERE idServicio = :id";
         $stmt = $this->db->prepare($query);
@@ -123,46 +84,21 @@ public function insertServicio($servicio, $descripcion,&$resultado)
         $stmt->bindValue(':descripcion', $descripcion, PDO::PARAM_STR);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
         return $stmt->execute();
-    }*/
-
-//Cuarto SP// 
-public function updateServicioServicio($id,$servicio, $descripcion,&$resultado)
-    {
-        $sql = "BEGIN updateServicio(:id,:servicio, :descripcion, :resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':servicio', $servicio);
-        $stmt->bindParam(':descripcion', $descripcion);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
 
 
 
 
 
-
-
-
-    /*public function deleteServicio($id)
+    public function deleteServicio($id)
     {
         $query = "DELETE FROM servicios WHERE idServicio = :id";
         $stmt = $this->db->prepare($query);
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
         return $stmt->execute();
-    }*/
-
-//Quinto SP// 
-public function deleteServicio($id,&$resultado)
-    {
-        $sql = "BEGIN deleteServicio(:id,:resultado); END;";
-        $stmt = $this->db->prepare($sql);
-
-        $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':resultado', $resultado, PDO::PARAM_INT, 1);
-        $stmt->execute();
     }
+
+
 
 
 
@@ -175,7 +111,7 @@ public function buscarServicios($searchTerm)
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-  //Sexto SP// 
+
 
 
 
